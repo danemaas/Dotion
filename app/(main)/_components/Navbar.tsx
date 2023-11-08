bf76c8db1399b9f8a9ec63bdd84585b2f3e06ab9
danemaas <danemaas16@gmail.com>
2023-11-08 15:46:20 +0800

    added features

--- app/(main)/_components/Navbar.tsx
+++ app/(main)/_components/Navbar.tsx
@@ -8,6 +8,7 @@ import { useParams } from "next/navigation";
 import Title from "./Title";
 import Banner from "./Banner";
 import Menu from "./Menu";
+import Publish from "./Publish";
 
 interface NavbarProps {
   isCollapsed: boolean;
@@ -52,6 +53,7 @@ const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
         <div className="flex items-center justify-between w-full">
           <Title initialData={document} />
           <div className="flex items-center gap-x-2">
+            <Publish initialData={document} />
             <Menu documentId={document._id} />
           </div>
         </div>
--- app/(public)/preview/[documentId]/page.tsx
+++ app/(public)/preview/[documentId]/page.tsx
@@ -54,10 +54,14 @@ const DocumentPage = ({ params }: DocumentPageProps) => {
 
   return (
     <div className="pb-40">
-      <CoverImage url={document.coverImage} />
+      <CoverImage preview url={document.coverImage} />
       <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
-        <Toolbar initialData={document} />
-        <Editor onChange={onChange} initialContent={document.content} />
+        <Toolbar preview initialData={document} />
+        <Editor
+          editable={false}
+          onChange={onChange}
+          initialContent={document.content}
+        />
       </div>
     </div>
   );
